bd0ccb7c0fa600b8e77f92b6d515bb0eb6fbc2a0
Bellatrix988 <marina.yurova1996@yandex.ru>
2017-10-03 10:38:25 +0300

    changed function with removeEventListener

--- app/js/count_click.js
+++ app/js/count_click.js
@@ -1,10 +1,11 @@
 let btn = document.getElementById('btn')
-                  .addEventListener('click', btnOncklick);
-var count = 0;
+                  .addEventListener("click", btnOnClick)
 
-function btnOncklick(){
-  count++;
-  if(count <= 3)
-    alert("Count click = "+ count);
+let count = 0;
+
+function btnOnClick(){
+  if(count < 3)
+    alert("Count click = " + ++count);
+  else
+    btn.removeEventListener("click", btnOnClick);
 }
-
