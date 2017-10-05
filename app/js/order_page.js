f23f675f25543a9329d3513cd16b44a16a35d33e
Bellatrix988 <marina.yurova1996@yandex.ru>
2017-10-05 12:30:32 +0300

    separated functions

--- app/js/order_page.js
+++ app/js/order_page.js
@@ -3,17 +3,16 @@ let indStuffing = document.getElementById("slStuffing").selectedIndex;
 
 let tastyHam = new Hamburger(BUFF_SIZE[indSize], BUFF_STUFFING[indStuffing]);
 
-function calc() {
+function createObj() {
     indSize = document.getElementById("slSize").selectedIndex;
     indStuffing = document.getElementById("slStuffing").selectedIndex;
 
     tastyHam = new Hamburger(BUFF_SIZE[indSize], BUFF_STUFFING[indStuffing]);
 
-    document.getElementById("outPrice").value = tastyHam.calculatePrice();
-    document.getElementById("outCalories").value = tastyHam.calculateCalories();
+    calc();
 };
 
-function reCulc(self) {
+function controllTopping(self) {
     let ind = self.value - 1;
     let topping = BUFF_TOPPING[ind];
     console.log(topping.price + ' ' + self.checked);
@@ -24,6 +23,14 @@ function reCulc(self) {
         tastyHam.removeTopping(topping);
         console.log('remove');
     }
+    calc();
+};
+
+function calc() {
     document.getElementById("outPrice").value = tastyHam.calculatePrice();
     document.getElementById("outCalories").value = tastyHam.calculateCalories();
-};
+}
+
+// function addOrder(){
+
+// }
